49bf4a74fada69be39100db72ab6e59ff039af4c
irina-mandarina <irina.ahamad@gmail.com>
2022-09-30 10:17:08 +0300

    should have commited the navigation guard changes to the router before i broke the css

--- src/router.js
+++ src/router.js
@@ -1,21 +1,33 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import store from './store.js'
 import LogIn from './components/LogIn.vue'
 import SignUp from './components/SignUp.vue'
 import NoteList from './components/NoteList.vue'
 import UserProfile from './components/UserProfile.vue'
-
-const notFound = { template: '<div> <h1> Not found </h1> </div>'}
+import NotFound from './components/NotFound.vue'
 
 const routes = [
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: notFound },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     { path: '/', redirect: '/login' },
     { path: '/login', name: 'Log In', component: LogIn },
     { path: '/signup', name: 'Sign Up', component: SignUp },
-    { path: '/notes', name: 'Notes', component: NoteList },
-    { path: '/profile', name: 'Profile', component: UserProfile }
+    { path: '/notes', name: 'Notes', component: NoteList, beforeEnter(to, from) {
+      if ( !store.state.logged ) {
+        redirect: '/login'
+      }
+      return store.state.logged
+    } },
+    { path: '/profile', name: 'Profile', component: UserProfile, beforeEnter(to, from) {
+      if ( !store.state.logged ) {
+        redirect: '/login'
+      }
+      return store.state.logged
+    } }
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+export default router
